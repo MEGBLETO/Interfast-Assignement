7db0f15cb450e85ddf1496e27023bd7a28c078ba
MEGBLETO carnel <carnelilordwi@gmail.com>
2021-02-08 19:53:18 +0100

    now time to add the shapes into the vackground

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 import { AiFillPlusCircle } from "react-icons/ai";
+import { BsArrowRight } from "react-icons/bs";
+import { GiBackwardTime } from "react-icons/gi";
+
+
 
 
 const Home = () => {
   return (
     <div className="showcase">
-      <Link to='/interventions'>
-        <button>Mes interventions</button>
+      <Link className="link" to='/interventions'>
+      <div className="button"><h1><BsArrowRight/></h1> Mes interventions </div>
       </Link>
 
-      <Link to='/interventions'>
-        <button>Mes interventions</button>
+      <Link className="link" to='/interventions'>
+        <div className="button"><h1><GiBackwardTime/></h1>Historiques</div>
       </Link>
       
       <h1 className='icone'><AiFillPlusCircle/></h1>
--- src/Components/Interventions.js
+++ src/Components/Interventions.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { AiFillPlusCircle } from "react-icons/ai";
+
 
 const Interventions = () => {
   return (
     <div className="showcase2">
-      
+ <h1 className='icone'><AiFillPlusCircle/></h1>
+
     </div>
   )
 }
